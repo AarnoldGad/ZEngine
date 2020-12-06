c868aa94932b83803793b62c9f42d27478184440
- The Aarnold - <manelgad@gmail.com>
2020-12-06 04:12:48 +0100

    Finishing Logger and Tee

--- TestField/TestField.cpp
+++ TestField/TestField.cpp
@@ -11,48 +11,8 @@
 
 #include <zengine/zengine.hpp>
 
-class Event
-{
-public:
-
-   virtual void bar() {}
-
-   template<typename From>
-   void foo(Event& event)
-   {
-      From* a = dynamic_cast<From*>(&event);
-      if (a == nullptr)
-         std::cout << "Not castable" << std::endl;
-      else
-         std::cout << "Castable" << std::endl;
-   }
-};
-
-class KeyEvent : public Event
-{
-
-};
-
-class MouseEvent : public Event
-{
-
-};
-
-class MouseButtonPressed : public MouseEvent
-{
-
-};
-
-class MouseButtonReleased : public MouseEvent
-{
-
-};
-
-
 int main()
 {
-   MouseButtonPressed a;
-   Event& b = a;
-   a.foo<Event>(b);
+
    return 0;
 }
