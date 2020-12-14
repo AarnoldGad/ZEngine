5dbd9af4adbdf3789a0c98c501e43f29c74c7acd
- The Aarnold - <manelgad@gmail.com>
2020-12-14 02:07:59 +0100

    Inlining definition in external file + Licence + Slight refactoring

--- ZEngine/include/zengine/Common/tee.hpp
+++ ZEngine/include/zengine/Common/tee.hpp
@@ -1,24 +1,15 @@
-#ifndef TEEDEBUG_HPP
-#define TEEDEBUG_HPP
-
-#include <iostream>
-#include <fstream>
-#include <functional>
-#include <filesystem>
-#include <type_traits>
-
 /*
  * tee.hpp
  * 31 Aug 2020
  * Gaétan "The Aarnold" Jalin
- * 
+ *
  * Copyright (C) 2020 Gaétan Jalin
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
  * arising from the use of this software.
  *
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
@@ -27,10 +18,10 @@
  *    claim that you wrote the original software. If you use this software
  *    in a product, an acknowledgment in the product documentation would be
  *    appreciated but is not required.
- * 
+ *
  * 2. Altered source versions must be plainly marked as such, and must not be
  *    misrepresented as being the original software.
- * 
+ *
  * 3. This notice may not be removed or altered from any source distribution.
  *
  * C++ 17
@@ -55,34 +46,31 @@
  * TODO Improve template deduction
  * TODO Improve template deduction
  */
+#ifndef ZE_TEEDEBUG
+#define ZE_TEEDEBUG
 
-//
-// Templated Functors Specialisation
-//
+template<typename Fn, typename Type = void>
+using if_is_invocable = std::enable_if_t<std::is_invocable_v<Fn>, Type>;
+
+template<typename Stream, typename Type = void>
+using if_is_stream = std::enable_if_t<std::is_base_of_v<std::ios_base, Stream>, Type>;
 
 // Primary specialisable template
 template<typename T, typename Enable = void>
 class Tee {};
 
 // Generic Customisable tee
-template<typename PrintFunc>
-class Tee<PrintFunc, std::enable_if_t<std::is_invocable_v<PrintFunc> > >
+template<typename PrintFn>
+class Tee<PrintFn, if_is_invocable<PrintFn> >
 {
 public:
+	template<typename Value>
+	decltype(auto) operator()(Value&& value);
 
-   explicit Tee(PrintFunc func)
-   : print(func) {}
-
-   template<typename T>
-   decltype(auto) operator()(T&& value)
-   {
-      print(value);
-      return std::forward<T>(value);
-   }
+	explicit Tee(PrintFn func);
 
 private:
-
-   PrintFunc print;
+	PrintFn m_print;
 };
 
 // File output specialisation
@@ -91,72 +79,42 @@ template<>
 class Tee<std::filesystem::path>
 {
 public:
+	template<typename Value>
+	decltype(auto) operator()(Value&& value);
 
-   explicit Tee(std::filesystem::path const& filePath, std::ios_base::openmode mode = std::ios_base::out | std::ios_base::app)
-   : m_file(filePath), m_mode(mode) {}
-
-   template<typename T>
-   decltype(auto) operator()(T&& value)
-   {
-      std::ofstream file(m_file, m_mode);
-
-      if (file)
-         file << value << std::endl;
-      else
-         std::cerr << "unable to open file " << m_file << std::endl;
-
-      return std::forward<T>(value);
-   }
+	explicit Tee(std::filesystem::path const& filePath, std::ios_base::openmode mode = std::ios_base::out | std::ios_base::app);
 
 private:
-
-   std::filesystem::path m_file;
-   std::ios_base::openmode m_mode;
+	std::filesystem::path m_file;
+	std::ios_base::openmode m_mode;
 };
 
 // Stream output specialisation
 template<typename Stream>
-class Tee<Stream, std::enable_if_t<std::is_base_of_v<std::ios_base, Stream> > >
+class Tee<Stream, if_is_stream<Stream> >
 {
 public:
+	template<typename Value>
+	decltype(auto) operator()(Value&& value);
 
-   explicit Tee(Stream& output)
-   : m_output(output) {}
-
-   template<typename T>
-   decltype(auto) operator()(T&& value)
-   {
-      m_output << value << std::endl;
-      return std::forward<T>(value);
-   }
+	explicit Tee(Stream& output);
 
 private:
-
-   std::ostream& m_output;
+	std::ostream& m_output;
 };
 
 // Deductions guides
 Tee(char const*) -> Tee<std::filesystem::path>;
 Tee(std::string const&) -> Tee<std::filesystem::path>;
 
-template<typename PrintFunc, std::enable_if_t<std::is_invocable_v<PrintFunc>, int> = 0>
-Tee<PrintFunc> make_tee(PrintFunc func)
-{
-   return Tee<PrintFunc>(func);
-}
+template<typename PrintFunc, if_is_invocable<PrintFunc, int> = 0>
+Tee<PrintFunc> make_tee(PrintFunc func);
 
-template<typename Stream, std::enable_if_t<std::is_base_of_v<std::ios_base, Stream>, int> = 0>
-Tee<Stream> make_tee(Stream& stream)
-{
-   return Tee<Stream>(stream);
-}
+template<typename Stream, if_is_stream<Stream, int> = 0>
+Tee<Stream> make_tee(Stream& stream);
 
 template<typename Value, typename TeeType>
-decltype(auto) operator|(Value&& value, Tee<TeeType> tee)
-{
-   tee(value);
-   return std::forward<Value>(value);
-}
+decltype(auto) operator|(Value&& value, Tee<TeeType> tee);
 
 // Default tee
 Tee<std::ostream> tee = make_tee(std::cout);
@@ -167,24 +125,12 @@ Tee<std::ostream> tee = make_tee(std::cout);
 
 // Simplest form
 template<typename Value, typename CharType>
-decltype(auto) stee(Value&& value, std::basic_ostream<CharType>& output)
-{
-   output << value << std::endl;
-   return std::forward<Value>(value);
-}
+decltype(auto) stee(Value&& value, std::basic_ostream<CharType>& output);
 
 // TODO Open a file at each call is performance heavy
 template<typename Value>
-decltype(auto) ftee(Value&& value, std::filesystem::path const& outputFile, std::ios_base::openmode mode = std::ios_base::out)
-{
-   std::ofstream file(outputFile, mode);
+decltype(auto) ftee(Value&& value, std::filesystem::path const& outputFile, std::ios_base::openmode mode = std::ios_base::out);
 
-   if (file)
-      file << value << std::endl;
-   else
-      std::cerr << "unable to open file " << outputFile << std::endl;
+#include <inline/Common/tee.inl>
 
-   return std::forward<Value>(value);
-}
-
-#endif // TEEDEBUG_HPP
+#endif // ZE_TEEDEBUG
