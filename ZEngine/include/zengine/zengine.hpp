7b3d47739a0ae059cd762e27dc72e94ddedcfb19
- The Aarnold - <manelgad@gmail.com>
2021-02-12 00:38:06 +0100

    I miss something...

--- ZEngine/include/zengine/zengine.hpp
+++ ZEngine/include/zengine/zengine.hpp
@@ -32,14 +32,14 @@
 #include <zengine/Core/Application.hpp>
 
 #include <zengine/Common/Logger.hpp>
-#include <zengine/Common/tee.hpp>
+#include <zengine/Common/Debug/tee.hpp>
 #include <zengine/Common/Time/Time.hpp>
 #include <zengine/Common/Time/Chrono.hpp>
 #include <zengine/Common/Time/Date.hpp>
 #include <zengine/Common/Signal/Signal.hpp>
 #include <zengine/Common/Signal/Connection.hpp>
 
-#include <zengine/Resources/ResourceManager.hpp>
+#include <zengine/Resource/ResourceManager.hpp>
 
 #include <zengine/Event/Event.hpp>
 #include <zengine/Event/EventBus.hpp>
